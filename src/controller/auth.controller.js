8947f484aec8b9226d5fd8b453defc23ed49bb64
Angel Juarez <spotify27816@gmail.com>
2023-12-11 17:56:32 -0600

    Nueva ruta api SameSite4

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -33,7 +33,7 @@ export const registrar = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: "None",
+      sameSite: "Lax",
       secure: true,
     });
     // res.json({
@@ -79,7 +79,7 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: "None",
+      sameSite: "Lax",
       secure: true,
     });
     // res.cookie("token", token); //guardamos en una cookie
